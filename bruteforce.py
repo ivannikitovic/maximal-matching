a3c1927b4fa27a0b01b2297d91118adcb5e81282
Ivan Nikitovic <ivan.bnikitovic@gmail.com>
2023-03-17 20:30:44 -0400

    add brute force algo

---
+++ bruteforce.py
@@ -0,0 +1,40 @@
+from graph import Graph
+
+import random
+
+def bruteforce(G: Graph):
+    """
+    
+    """
+
+    edges = G.get_all_edges()
+    random.shuffle(edges)
+
+    M = set()
+
+    for i in range(len(edges)):
+        idx1, idx2 = edges[i]
+
+        shares_endpoint = False
+        for edge in M:
+            if idx1 in edge or idx2 in edge:
+                shares_endpoint = True
+                break
+
+        if not shares_endpoint:
+            M.add(frozenset(edges[i]))
+
+    return [list(edge) for edge in M]
+
+if __name__ == "__main__":
+    test = Graph(5)
+
+    test.adj[0] = [0, 1, 0, 0, 1]
+    test.adj[1] = [1, 0, 1, 1, 1]
+    test.adj[2] = [0, 1, 0, 1, 0]
+    test.adj[3] = [0, 1, 1, 0, 1]
+    test.adj[4] = [1, 1, 0, 1, 0]
+
+    print(test)
+
+    print(bruteforce(test))
